d39389b8f7d9dab6ddc4157bf4977f497919e893
Orisabiyi <orisabiyidavid@gmail.com>
2024-03-08 13:14:47 +0100

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+const navBar = document.querySelector('.nav')
+const navBarList = document.querySelector('.nav__list')
+
+const toggleNavBar = function (e) {
+  const btn = e.target.closest('.nav__menu')
+  if (!btn) return;
+
+  navBarList.classList.toggle('nav__list--toggle')
+}
+
+navBar.addEventListener('click', toggleNavBar)
